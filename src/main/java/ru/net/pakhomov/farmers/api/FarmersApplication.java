1fc5051f9c523221059f90a152f8ce0f204afc02
anatoliy <pahomovt@gmail.com>
2023-07-09 17:58:48 +0600

    Changes for ready to deploy

--- src/main/java/ru/net/pakhomov/farmers/api/FarmersApplication.java
+++ src/main/java/ru/net/pakhomov/farmers/api/FarmersApplication.java
@@ -2,9 +2,10 @@ package ru.net.pakhomov.farmers.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FarmersApplication {
+public class FarmersApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(FarmersApplication.class, args);
