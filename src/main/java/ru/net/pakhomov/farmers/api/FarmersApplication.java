0e1a69d497656ac66429dc6d60299358ad09b0be
anatoliy <pahomovt@gmail.com>
2023-07-09 18:41:30 +0600

    Ready to start in localhost

--- src/main/java/ru/net/pakhomov/farmers/api/FarmersApplication.java
+++ src/main/java/ru/net/pakhomov/farmers/api/FarmersApplication.java
@@ -2,10 +2,9 @@ package ru.net.pakhomov.farmers.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class FarmersApplication extends SpringBootServletInitializer {
+public class FarmersApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(FarmersApplication.class, args);
